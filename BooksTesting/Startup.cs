b5a66308fc741782cb435f4c3488e05d12f0f645
Ahmed Kandeel <ahmed-kandeel@outlook.com>
2019-04-13 14:47:54 +0200

    add solution

---
+++ BooksTesting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BooksTesting.Startup))]
+namespace BooksTesting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
